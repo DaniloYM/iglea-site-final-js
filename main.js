f94098e9fa66b369f79f92fa243b08b8e25bd3cb
Danilo Yuji Matubara <104095038+DaniloYM@users.noreply.github.com>
2023-11-23 11:15:47 -0300

    ab
    ab

---
+++ main.js
@@ -0,0 +1,39 @@
+function tocaSom (selectorAudio) {
+  const elemento = document.querySelector(selectorAudio).play();
+}
+
+if(elemento !=null && elemento.localName === 'audio');{
+  elemento.play();
+}
+else {
+//alert('Elemento não encontrado')
+console.log('Elemento não encontrado ou seletor inválido')
+}
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`; //template string
+
+  tecla.onclick = function () {
+      tocaSom(idAudio);
+  }
+
+}
+
+tecla.onkeydown = function (evento){
+
+  if (evento.code === 'Space' || evento.code === 'Enter') {
+  tecla.classiList.add('ativa')
+}
+
+}
+
+tecla.onkeyup = function() {
+  tecla.classList.remove('')
+}
